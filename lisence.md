36b78acc9668c4933ff6246e2d74bab87bb735c3
Kaleb Franken <kaleb.franken7@gmail.com>
2023-06-10 21:31:55 -0600

    commit

---
+++ lisence.md
@@ -0,0 +1,1 @@
+"tron disk" (https://skfb.ly/68ZwL) by Medeixo is licensed under Creative Commons Attribution (http://creativecommons.org/licenses/by/4.0/).
--- src/App.tsx
+++ src/App.tsx
@@ -1,46 +1,53 @@
-import { useEffect, useState } from 'react'
+import { useEffect } from 'react';
+
 import * as THREE from 'three';
-import './App.css'
+// import { FBXLoader } from 'three/examples/jsm/loaders/FBXLoader';
+// import { OBJLoader } from 'three/examples/jsm/loaders/OBJLoader';
+// import { VOXLoader } from 'three/examples/jsm/loaders/VOXLoader';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
+
+import SceneInit from './lib/SceneInit';
 
 function App() {
   useEffect(() => {
-    
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+    const test = new SceneInit('myThreeJsCanvas');
+    test.initialize();
+    test.animate();
 
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
-document.body.appendChild( renderer.domElement );
+    // const boxGeometry = new THREE.BoxGeometry(8, 8, 8);
+    // const boxMaterial = new THREE.MeshNormalMaterial();
+    // const boxMesh = new THREE.Mesh(boxGeometry, boxMaterial);
+    // test.scene.add(boxMesh);
 
-const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-const cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
+    let loadedModel:any;
+    const glftLoader = new GLTFLoader();
+    glftLoader.load('../models/tron_disk/scene.gltf', (gltfScene) => {
+      loadedModel = gltfScene;
+      // console.log(loadedModel);
 
-camera.position.z = 5;
+      gltfScene.scene.rotation.y = Math.PI / 8;
+      // gltfScene.scene.position.y = 30;
+      // gltfScene.scene.position.z = 30;
+      gltfScene.scene.scale.set(0.2, 0.2, 0.2);
+      test.scene.add(gltfScene.scene);
+    });
 
-function animate() {
-	requestAnimationFrame( animate );
-
-	cube.rotation.x += 0.01;
-	cube.rotation.y += 0.01;
-
-	renderer.render( scene, camera );
-}
-
-animate();
-
-
-  }, [])
-  
+    const animate = () => {
+      if (loadedModel) {
+        loadedModel.scene.rotation.x += 0.01;
+        loadedModel.scene.rotation.y += 0.01;
+        loadedModel.scene.rotation.z += 0.01;
+      }
+      requestAnimationFrame(animate);
+    };
+    animate();
+  }, []);
 
   return (
-    <>
-      <div className='text-bold' id='myThreeJsCanvas'>
-        <canvas />
-      </div>
-    </>
-  )
+    <div>
+      <canvas id="myThreeJsCanvas" />
+    </div>
+  );
 }
 
-export default App
+export default App;
